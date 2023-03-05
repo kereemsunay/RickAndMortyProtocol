60c11f609894e37a731c6ec935bfe1b64ec72e6a
kereemsunay <sunay.kerem@outlook.com>
2023-03-05 22:01:01 +0300

    Initial Commit

---
+++ RickAndMortyProtocol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyProtocol
+//
+//  Created by Kerem on 5.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
